cae6afaae5e4b5c1a0e0148b5145e9c5519d0f2a
Enema Onojah John <54289623+Enema001@users.noreply.github.com>
2019-09-26 16:58:25 +0100

    Teacher add Items to class

---
+++ realIndex.js
@@ -0,0 +1,9 @@
+
+function myFunction(){
+      var inputVal = document.getElementById('userInput').value;
+       document.getElementById('list').innerHTML= inputVal;
+}
+
+
+var items = []
+items.push(inputVal)
